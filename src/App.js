018758391b85dde72734790945048281e10cbdf2
Ryan Stong <rvanstong@gmail.com>
2017-04-25 19:15:28 -0700

    finishes lesson 9 of egghead react lessons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 import React from 'react';
 
-const App = () => <h1>Hello</h1>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {currentEvent: '---'}
+    this.update = this.update.bind(this)
+  }
+
+  update(e){
+    this.setState({currentEvent: e.type})
+  }
+
+  render(){
+    return (
+      <div>
+        <textarea
+          onKeyPress={this.update}
+          onCopy={this.update}
+          onCut={this.update}
+          onPaste={this.update}
+          onFocus={this.update}
+          onBlur={this.update}
+          onDouble
+          col="30"
+          rows="10" />
+        <h1>{this.state.currentEvent}</h1>
+      </div>
+    )
+  }
+}
+
 
 export default App
