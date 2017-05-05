b1e149920074f385a1021e47b4a8306c74d9f734
Ryan Stong <rvanstong@gmail.com>
2017-05-05 13:49:58 -0700

    adds remove function to multi-die

--- projects/multi-die/src/App.js
+++ projects/multi-die/src/App.js
@@ -34,7 +34,7 @@ class Die extends Component {
     let dieSides = this.state.dieSides
     let dieFace = this.state.dieFace
     return (
-      <div className={`die d${dieSides}`}>
+      <div>
       <h1>{dieFace}</h1>
       <button onClick={this.update.bind(this)}>roll d{dieSides}</button>
       </div>
@@ -47,18 +47,23 @@ class DiceCup extends Component {
     super(props);
     this.state = {
       dice: [],
-      nextAddDieSides: 6
+      nextAddDieSides: 6,
     }
   }
 
   addDie = (sides) => {
+    console.log(`d${sides}`)
     let diceSet = this.state.dice
     diceSet.push(sides)
     this.setState({dice: diceSet})
   }
 
-  update() {
-    this.addDie(this.state.nextAddDieSides)
+  removeDie = (dieIndex) => {
+    console.log(`removing die at ${dieIndex}`)
+    let diceSet = this.state.dice
+    diceSet.splice(dieIndex, 1)
+    console.log(diceSet)
+    this.setState({dice: diceSet})
   }
 
   render() {
@@ -68,14 +73,15 @@ class DiceCup extends Component {
       <div id='die-adder'>
         <h4>Specify number of die faces</h4>
         <input type='number' value={this.state.nextAddDieSides} onChange={event => this.setState({ nextAddDieSides: event.target.value })} />
-        <button id='add-die' onClick={this.update.bind(this)}>Add Dice</button>
+        <button id='add-die' onClick={() => {this.addDie(this.state.nextAddDieSides)}}>Add Dice</button>
       </div>
       <ul id="dice-cup">
       {
         dice.map((die, index) => {
           return (
-            <li key={`d${die}_${index}`}>
-            <Die dieSides={die}/>
+            <li key={`d${die}_${index}`} className={`die d${die}`}>
+              <button className='remove-die' onClick={() => {this.removeDie(index)}}>x</button>
+              <Die dieSides={die}/>
             </li>
             );
         })
