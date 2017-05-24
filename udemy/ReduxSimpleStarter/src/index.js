9f3eb7d1a989aa5ac65a82155f37d694e8aa5b49
Ryan Stong <rvanstong@gmail.com>
2017-05-24 10:45:28 -0700

    adds in search bar to redux simple starter

--- udemy/ReduxSimpleStarter/src/index.js
+++ udemy/ReduxSimpleStarter/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+
+const API_KEY = ''
+
 const App = () => {
   return (
-    <div>Hi!</div>
-  )
-}
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
