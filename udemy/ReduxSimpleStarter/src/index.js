1a13defd67feb38a812505a0526198f7ee9e34ab
Ryan Stong <rvanstong@gmail.com>
2017-06-06 11:59:58 -0700

    imports YTSearch

--- udemy/ReduxSimpleStarter/src/index.js
+++ udemy/ReduxSimpleStarter/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar';
 
 
 const API_KEY = ''
+
+YTSearch({key: API_KEY, term: 'wat'}, function(data) {
+  console.log(data)
+})
 
 const App = () => {
   return (
