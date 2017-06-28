71b2dcd662d6747709a7688950a15e1b932e8a8f
Ryan Stong <rvanstong@gmail.com>
2017-06-28 11:29:17 -0700

    search bar now renders search

--- udemy/ReduxSimpleStarter/src/index.js
+++ udemy/ReduxSimpleStarter/src/index.js
@@ -19,10 +19,14 @@ class App extends Component {
       selectedVideo: null,
      };
 
-    YTSearch({key: API_KEY, term: 'wat'}, (videos) => {
+     this.videoSearch('wat')
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
-        selectedVideo: videos[0],
+        selectedVideo: videos[0]
        });
     })
   }
@@ -30,7 +34,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={(term) => this.videoSearch(term) }/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
